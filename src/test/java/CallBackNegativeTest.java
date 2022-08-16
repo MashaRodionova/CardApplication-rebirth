5b23736633f273de34a3065217961ceb7b4168e3
Masha Rodionova <rodionovamv@gmai.com>
2022-08-16 13:20:03 +0300

    Added port 7777 in appveyor.yml

--- src/test/java/CallBackNegativeTest.java
+++ src/test/java/CallBackNegativeTest.java
@@ -48,8 +48,8 @@ class CallbackNegativeTest {
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("Маша");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.className("button_theme_alfa-on-white")).click();
-        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        String actual = driver.findElement(By.className("input__sub")).getText();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=phone].input__sub")).getText();
         Assertions.assertEquals(expected, actual);
 
     }
